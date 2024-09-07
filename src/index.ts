5a8b242540bda957d7cc465857805cbcf141628b
aka <akaseeingred@gmail.com>
2024-09-07 20:11:19 +0400

    lnk files and gog support

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,22 @@
-// import * as ws from 'windows-shortcuts'
 import { getGamePath } from 'steam-game-path'
 import fs from 'fs'
 // @ts-ignore
 import createDesktopShortcut from 'create-desktop-shortcuts'
+// @ts-ignore
+import getWindowsShortcutProperties from 'get-windows-shortcut-properties'
 import * as path from 'path'
 
 type FileData = {
-    uri: string;
-    icon: string;
+    uri: string
+    icon: string
 }
 
 enum StoreType {
     STEAM = 'STEAM',
     EGS = 'EGS',
+    GOG = 'GOG',
     STANDALONE = 'STANDALONE'
 }
-
-// const queryShortcut = (shortcut: string) => {
-//     const promise = new Promise((resolve, reject) => {
-//         const cb = (error: string | null, options?: ws.ShortcutOptions) => {
-//             if (error) {
-//                 reject(error)
-//             }
-//             console.log(options)
-//             resolve(options)
-//         }
-//         try {
-//             ws.query('C:/ProgramData/Microsoft/Windows/Start Menu/Windows Update.lnk', cb)
-//         } catch (err) {
-//             reject(err)
-//         }
-//     })
-//     return promise
-// }
 
 // function to find exe file in a directory
 function findExeFile(dir: string): string | null {
@@ -45,17 +29,57 @@ function findExeFile(dir: string): string | null {
     return null
 }
 
+function findLaunchLnk(dir: string): string | null {
+    const files = fs.readdirSync(dir)
+    for (const file of files) {
+        if (path.extname(file) === '.lnk' && file.includes('Launch')) {
+            return path.join(dir, file)
+        }
+    }
+    return null
+}
+
+type LnkProperties = {
+    FullName: string
+    Arguments: string
+    Description: string
+    Hotkey: string
+    IconLocation: string
+    RelativePath: string
+    TargetPath: string
+    WindowStyle: string
+    WorkingDirectory: string
+}
+
 const main = async (chihuahuaPath: string, sourceFile: string) => {
     const sourceFileExtension = sourceFile.substring(sourceFile.lastIndexOf('.'))
     const sourceFileName = sourceFile.substring(sourceFile.lastIndexOf('\\') + 1)
     const sourceFileNameWithoutExtension = sourceFileName.substring(0, sourceFileName.lastIndexOf('.'))
+
     let fileData: FileData = {
         uri: '',
         icon: ''
     }
+
+    const currentDir = process.cwd()
+    const chihuahuaDir = chihuahuaPath.substring(0, chihuahuaPath.lastIndexOf('\\')) + '\\'
+
+    const options = {
+        filePath: chihuahuaPath,
+        name: sourceFileNameWithoutExtension,
+        comment: 'Launch in UEVR',
+        workingDir: chihuahuaDir,
+        icon: '',
+        outputPath: currentDir,
+        arguments: ''
+    }
+
+    let shortcutData: LnkProperties | null = null
+
     if (sourceFileExtension === '.lnk') {
-        // const shortcutData = await queryShortcut(sourceFile)
-        throw new Error('Not implemented YET')
+        const result = getWindowsShortcutProperties.sync(sourceFile)
+        console.log(`shortcutData:  `, result)
+        shortcutData = result[0] as LnkProperties
     } else if (sourceFileExtension === '.url') {
         const fileContent = fs.readFileSync(sourceFile, 'utf8')
         const fileLines = fileContent.split('\n')
@@ -74,7 +98,6 @@ const main = async (chihuahuaPath: string, sourceFile: string) => {
             icon
         }
     }
-    const chihuahuaDir = chihuahuaPath.substring(0, chihuahuaPath.lastIndexOf('\\')) + '\\'
 
     let store: StoreType = StoreType.STANDALONE
     if (sourceFileExtension === '.url' && fileData.uri.startsWith('steam://')) {
@@ -82,20 +105,9 @@ const main = async (chihuahuaPath: string, sourceFile: string) => {
     } else if (sourceFileExtension === '.url' && fileData.uri.startsWith('com.epicgames.launcher://')) {
         store = StoreType.EGS
     } else if (sourceFileExtension === '.lnk') {
-        throw new Error('Not implemented YET')
-    }
-
-    const currentDir = process.cwd()
-
-    const options = {
-        filePath: chihuahuaPath,
-        name: sourceFileNameWithoutExtension,
-        comment: 'Launch in UEVR',
-        workingDir: chihuahuaDir,
-        icon: fileData.icon,
-        outputPath: currentDir,
-        arguments: ''
-        // iconIndex: 0
+        if (shortcutData?.TargetPath.includes('GalaxyClient.exe')) {
+            store = StoreType.GOG
+        }
     }
 
     if (store === StoreType.STEAM) {
@@ -107,21 +119,37 @@ const main = async (chihuahuaPath: string, sourceFile: string) => {
         const gameName = gamePathObj?.game?.name || sourceFileNameWithoutExtension
         options.arguments = `"${exeFilePath}" --launch-cmd "${fileData.uri}"`
         options.name = gameName
+        options.icon = fileData.icon
     } else if (store === StoreType.EGS) {
         // gamePath in EGS is usually the same as icon path
         const exeFilePath = fileData.icon
-        options.arguments = `${exeFilePath} --launch-cmd "${fileData.uri}"`
+        options.arguments = `"${exeFilePath}" --launch-cmd "${fileData.uri}"`
+        options.icon = fileData.icon
+    } else if (store === StoreType.GOG && shortcutData) {
+        const shortcutDataEnsured = shortcutData as LnkProperties
+        const args = shortcutDataEnsured.Arguments
+        const gamePathUri = args.substring(args.indexOf('/path=') + '/path="'.length, args.lastIndexOf('"'))
+        const properLnk = findLaunchLnk(gamePathUri)
+        shortcutData = getWindowsShortcutProperties.sync(properLnk)[0] as LnkProperties
+        console.log(`shortcutData real:  `, shortcutData);
+        options.arguments = `"${shortcutData.TargetPath}"${shortcutData.Arguments? ` --launch-args "${shortcutData.Arguments}"` : ''}`
+        options.icon = shortcutData.IconLocation || ''
+
+    } else if (shortcutData) {
+        const shortcutDataEnsured = shortcutData as LnkProperties
+        options.arguments = `"${shortcutData.TargetPath}"${shortcutDataEnsured.Arguments? ` --launch-args "${shortcutDataEnsured.Arguments}"` : ''}`
+        options.icon = shortcutDataEnsured.IconLocation || ''
     }
 
+    options.name = options.name + ' [ue]'
+
     console.log(`LNK FILE OPTIONS:  `, options)
-    const result = await createDesktopShortcut({
+
+    await createDesktopShortcut({
         windows: options
     })
-    // console.log(`result:  `, result)
 }
 
 const chihuahuaPath = process.argv[2]
-// console.log(`chihuahuaPath:  `, chihuahuaPath)
 const sourceLnkFile = process.argv[3]
-// console.log(`sourceLnkFile:  `, sourceLnkFile)
 main(chihuahuaPath, sourceLnkFile)
